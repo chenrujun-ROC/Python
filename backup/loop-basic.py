9d008f5d1e9e3960fc1bd4e20228e118d094c0db
chenrujun-ROC <84781971+chenrujun-ROC@users.noreply.github.com>
2021-07-10 13:34:25 +0800

    Create loop-basic.py

---
+++ backup/loop-basic.py
@@ -0,0 +1,26 @@
+# while迴圈
+n=1
+sum=0
+while n<=10:
+    sum=sum+n
+    n+=1
+    print(sum)
+# for迴圈
+#印出列表
+for x in [3,5,1,6]:
+    print(x)
+#印出字串中個別字元
+for x in "hello":
+    print(x)
+#印出0-4的列表
+for x in range(5):
+    print(x)
+#印出5-(10-1)的列表
+for x in range(5,10):
+    print(x)
+#1+2+..+9+10
+sum=0
+for x in range(1,11):
+    sum=sum+x
+print(sum)
+
