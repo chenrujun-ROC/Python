87910f7ba3daad33c19ca37cf6d8189fa4dff13a
chenrujun-ROC <84781971+chenrujun-ROC@users.noreply.github.com>
2021-07-12 00:55:57 +0800

    Update pandas-dataframe.py

--- pandas-dataframe.py
+++ pandas-dataframe.py
@@ -9,26 +9,26 @@ data=pd.DataFrame({
 print(data)
 print("============")
 
-# # 觀察資料
-# print("資料數量",data.size)
-# print("資料形狀(列)(攔)",data.shape)
-# print("資料索引",data.index)
+# 觀察資料
+print("資料數量",data.size)
+print("資料形狀(列)(攔)",data.shape)
+print("資料索引",data.index)
 
 # 取得列 (Row/橫向) 的 Series 資料: 根據順序 根據索引
-# print("取得第二列", data.iloc[1], sep="\n")           # 取得第 二 列
-# print("============")
-# print("取得第c列", data.loc["c"], sep="\n")           # 取得第 三 列
+print("取得第二列", data.iloc[1], sep="\n")           # 取得第 二 列
+print("============")
+print("取得第c列", data.loc["c"], sep="\n")           # 取得第 三 列
 
 # 取得欄 (Column/直向) 的 Series 資料: 根據順欄位名稱
-# print("取得 name 欄位", data["name"], sep="\n")         # 取得 name 欄
-# print("取得 salary 欄位", data["salary"], sep="\n")     # 取得 salary 欄
+print("取得 name 欄位", data["name"], sep="\n")         # 取得 name 欄
+print("取得 salary 欄位", data["salary"], sep="\n")     # 取得 salary 欄
 
-# names=data["name"]
-# print("把 name 全部轉大寫", names.str.upper(),sep="\n")
+names=data["name"]
+print("把 name 全部轉大寫", names.str.upper(),sep="\n")
 
-# # 計算薪水的平均值
-# salaries=data["salary"]
-# print("薪水的平均值", salaries.mean
+# 計算薪水的平均值
+salaries=data["salary"]
+print("薪水的平均值", salaries.mean)
 
 # 建立新的欄位
 data["revenue"]=[500000, 400000, 300000]                # data[新欄位的名稱]=列表
